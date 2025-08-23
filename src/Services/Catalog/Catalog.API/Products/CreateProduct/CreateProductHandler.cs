f943b8cba1f6a08fa9f5bb348e7a14e91ad96fac
khoiphanngoc89 <30396127+khoiphanngoc89@users.noreply.github.com>
2025-08-23 18:15:46 +0700

    implement CreateProduct and refactor

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,4 +1,6 @@
 using BuildingBlocks.Common.Cqrs;
+using Catalog.API.Models;
+using Marten;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -7,10 +9,23 @@ public record CreateProductCommand(string Name, List<string> Category, string De
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler
+internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+        
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
---
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,42 @@
+using Marten;
+using Weasel.Core;
+
+namespace Catalog.API;
+
+public static class Startup
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+        builder.Services.AddCarter();
+        builder.Services.AddMediatR(conf =>
+        {
+            conf.RegisterServicesFromAssembly(typeof(Program).Assembly);
+        });
+        builder.Services.AddMarten(opts =>
+        {
+            opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+        }).UseLightweightSessions();
+        
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        
+        return builder.Build();;
+    }
+
+    public static WebApplication AddPipeline(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        
+        app.UseHttpsRedirection();
+        
+        app.MapCarter();
+        return app;
+    }
+}
