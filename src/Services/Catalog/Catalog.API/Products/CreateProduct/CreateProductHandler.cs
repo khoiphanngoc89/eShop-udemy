10a07666e2218d655015ddbefaf2b52c8f1fd5de
Khoi Phan <khoiphanngoc89@gmail.com>
2025-08-23 21:06:22 +0700

    add docker-compose for postgres and get product handler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -4,7 +4,7 @@ using Marten;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) :
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) :
     ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
@@ -17,7 +17,7 @@ internal class CreateProductCommandHandler(IDocumentSession session)
         var product = new Product
         {
             Name = command.Name,
-            Category = command.Category,
+            Categories = command.Categories,
             Description = command.Description,
             ImageFile = command.ImageFile,
             Price = command.Price
@@ -26,6 +26,6 @@ internal class CreateProductCommandHandler(IDocumentSession session)
         session.Store(product);
         await session.SaveChangesAsync(cancellationToken);
         
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }
